46822097aff575ee9a096c8bd2d6ed4ae9ae8267
Pedro Nunes <pedro.icn88@gmail.com>
2022-08-26 15:07:40 -0300

    Configurando redux

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,13 @@
 // Coloque aqui suas actions
+export const GET_USER_INFORMATION = 'GET_USER_INFORMATION';
+export const MANAGE_WALLET = 'MANAGE_WALLET';
+
+export const getUserAction = (payload) => ({
+  type: GET_USER_INFORMATION,
+  payload,
+});
+
+export const manageWalletAction = (payload) => ({
+  type: GET_USER_INFORMATION,
+  payload,
+});
