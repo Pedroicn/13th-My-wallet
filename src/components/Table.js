4606df57b5888e50a2a2b204abb7f1e093e8477b
Pedro Nunes <pedro.icn88@gmail.com>
2022-09-01 15:32:31 -0300

    Requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,27 @@ import React, { Component } from 'react';
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <section>
+        <caption>
+          <h3>Despesas</h3>
+        </caption>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+
+        </table>
+      </section>
     );
   }
 }
